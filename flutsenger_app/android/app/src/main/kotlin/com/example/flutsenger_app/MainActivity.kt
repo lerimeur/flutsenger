c548e0dc27ce0b4d08caef1c997cfe8579e9dfa3
Lerimeur <gregoirebrasseurpro@gmail.com>
2021-10-23 22:34:16 -0700

    rebase repo

--- flutsenger_app/android/app/src/main/kotlin/com/example/flutsenger_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutsenger_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutsenger_app/lib/utilities/useful_fct.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-void goOn(Widget value, BuildContext context) {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => value));
-}
